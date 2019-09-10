d18f4b970f995e4c193c4f96c99ac699d64b8acb
Aleksandar ALEKSOV <aaleksov@e1r1p1.42.fr>
2019-09-10 09:34:10 +0200

    Sully.s termine donc ASM FINI

--- ASM/Sully/Sully.s
+++ ASM/Sully/Sully.s
@@ -1,9 +1,7 @@
 section .data
-s db "fichier Sully_%d.s", 0
-command db "nasm -f macho64 Sully_%d.s && ld -lSystem -macosx_version_min 10.12 -o Sully_%d Sully_%d.o && rm *.o && ./Sully_%d", 0
+s db "section .data%2$cs db %3$c%1$s%3$c, 0%2$ccommand db %3$cnasm -f macho64 Sully_%%1$d.s && ld -lSystem -macosx_version_min 10.12 -o Sully_%%1$d Sully_%%1$d.o && rm *.o && ./Sully_%%1$d%3$c, 0%2$cfichier db %3$cSully_%%d.s%3$c, 0%2$csection .text%2$cglobal _main%2$cextern _sprintf%2$cextern _dprintf%2$cextern _malloc%2$cextern _free%2$cextern _system%2$cextern _printf%2$cextern _exit%2$cextern _access%2$c_main:%2$cpush rbp%2$cmov rbp, rsp%2$cmov r15, %4$d%2$ccmp r15, 1%2$cjb end%2$cmov rdi, 10%2$ccall _malloc%2$cmov r14, rax%2$cmov rdi, r14%2$cmov rsi, fichier%2$cmov rdx, r15%2$ccall _sprintf%2$csub  rsp, 8%2$ccall checker%2$cadd rsp, 8%2$cmov rdi, 10%2$ccall _malloc%2$cmov r14, rax%2$cmov rdi, r14%2$cmov rsi, fichier%2$cmov rdx, r15%2$ccall _sprintf%2$cmov rax, 0x2000005%2$cmov rdi, r14%2$cmov rsi, 514%2$cmov rdx, 0644o%2$csyscall%2$cmov rdi, rax%2$cmov rsi, s%2$cmov rdx, s%2$cmov rcx, 10%2$cmov r8, 34%2$cmov r9, r15%2$ccall _dprintf%2$cmov rdi, r14%2$ccall _free%2$cmov rdi, 255%2$ccall _malloc%2$cmov r14, rax%2$cmov rdi, r14%2$cmov rsi, command%2$cmov rdx, r15%2$ccall _sprintf%2$cmov rdi, r14%2$ccall _system%2$cmov rdi, r14%2$ccall _free%2$ccall end%2$cchecker:%2$clea rdi, [rel r14]%2$cmov rsi, 0%2$ccall _access%2$ccmp rax, 0%2$cjbe file_exist%2$cmov rdi, r14%2$ccall _free%2$cret%2$cfile_exist:%2$cdec r15%2$cret%2$cend:%2$cmov rax, 0x2000001%2$cxor rdi, rdi%2$csyscall", 0
+command db "nasm -f macho64 Sully_%1$d.s && ld -lSystem -macosx_version_min 10.12 -o Sully_%1$d Sully_%1$d.o && rm *.o && ./Sully_%1$d", 0
 fichier db "Sully_%d.s", 0
-format db "%s", 10
-testt db "testt", 10
 section .text
 global _main
 extern _sprintf
@@ -12,14 +10,14 @@ extern _malloc
 extern _free
 extern _system
 extern _printf
+extern _exit
+extern _access
 _main:
 push rbp
 mov rbp, rsp
-mov r15, 5
-
+mov r15, 10
 cmp r15, 1
 jb end
-
 mov rdi, 10
 call _malloc
 mov r14, rax
@@ -27,46 +25,50 @@ mov rdi, r14
 mov rsi, fichier
 mov rdx, r15
 call _sprintf
-
+sub  rsp, 8
 call checker
-
-; mov rdi, 10
-; call _malloc
-; mov r14, rax
-; mov rdi, r14
-; mov rsi, fichier
-; mov rdx, r15
-; call _sprintf
-
-; mov rax, 0x2000005
-; mov rdi, r14
-; mov rsi, 514
-; mov rdx, 0644o
-; syscall
-
-; mov rdi, rax
-; mov rsi, s
-; mov rdx, r15
-; call _dprintf
-
-; mov rdi, r14
-; call _free
-
+add rsp, 8
+mov rdi, 10
+call _malloc
+mov r14, rax
+mov rdi, r14
+mov rsi, fichier
+mov rdx, r15
+call _sprintf
+mov rax, 0x2000005
+mov rdi, r14
+mov rsi, 514
+mov rdx, 0644o
+syscall
+mov rdi, rax
+mov rsi, s
+mov rdx, s
+mov rcx, 10
+mov r8, 34
+mov r9, r15
+call _dprintf
+mov rdi, r14
+call _free
+mov rdi, 255
+call _malloc
+mov r14, rax
+mov rdi, r14
+mov rsi, command
+mov rdx, r15
+call _sprintf
+mov rdi, r14
+call _system
+mov rdi, r14
+call _free
 call end
 checker:
-mov rdi, format
-mov rsi, testt
-mov rax, 0
-call _printf
-; mov rax, 0x2000021
-; mov rdi, r14
-; mov rsi, 0
-; syscall
-; cmp rax, 0x0
-; jae file_exist
-; mov rdi, r14
-; call _free
-mov rax, 0
+lea rdi, [rel r14]
+mov rsi, 0
+call _access
+cmp rax, 0
+jbe file_exist
+mov rdi, r14
+call _free
 ret
 file_exist:
 dec r15
